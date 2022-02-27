08fd9759372702268f9c56e7ee1c793ed7c2cea1
eebalboni <emilyebalboni@gmail.com>
2022-02-27 00:12:22 -0500

    adding ascii

--- src/Animal.java
+++ src/Animal.java
@@ -10,16 +10,16 @@ public class Animal {
         userInput = scan.next();
         do {
             if (userInput.toLowerCase().equals("cat")) {
-                System.out.println("Print ASCII cat here.");
+                System.out.println(" |\\__/,|   (`\\\n |_ _  |.--.) )\n ( T   )     /\n(((^_(((/(((_/");
             } else if (userInput.toLowerCase().equals("dog")) {
-                System.out.println("Print ASCII dog here");
+                System.out.println("            __\n(\\,--------\'()\'--o\n (_    ___    /~\"\n  (_)_)  (_)_)");
             }else{
                 System.out.println("Error: incorrect user input. Would you like to see a cat or dog?");
                 userInput = scan.next();
                 if (userInput.toLowerCase().equals("cat")) {
-                    System.out.println("Print ASCII cat here.");
+                    System.out.println(" |\\__/,|   (`\\\n |_ _  |.--.) )\n ( T   )     /\n(((^_(((/(((_/");
                 } else if (userInput.toLowerCase().equals("dog")) {
-                    System.out.println("Print ASCII dog here");
+                    System.out.println("            __\n(\\,--------\'()\'--o\n (_    ___    /~\"\n  (_)_)  (_)_)");
                 }
             }
         } while (!userInput.toLowerCase().equals("cat") && !userInput.toLowerCase().equals("dog")) ;
