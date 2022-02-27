ce6740b0d5952d9171fa3528f1d9fff0b28aa67f
eebalboni <emilyebalboni@gmail.com>
2022-02-27 09:22:23 -0500

    adding fish option

--- src/Animal.java
+++ src/Animal.java
@@ -6,23 +6,27 @@ public class Animal {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Would you like to see a cat or dog?");
+        System.out.println("Would you like to see a cat or dog or fish?");
         userInput = scan.next();
         do {
             if (userInput.toLowerCase().equals("cat")) {
                 System.out.println(" |\\__/,|   (`\\\n |_ _  |.--.) )\n ( T   )     /\n(((^_(((/(((_/");
             } else if (userInput.toLowerCase().equals("dog")) {
                 System.out.println("            __\n(\\,--------\'()\'--o\n (_    ___    /~\"\n  (_)_)  (_)_)");
-            }else{
+            }else if(userInput.toLowerCase().equals("fish")){
+                System.out.println("     o                 o\n                  o\n         o   ______      o\n           _/  (   \\_\n _       _/  (       \\_  O\n| \\_   _/  (   (    0  \\\n|== \\_/  (   (          |\n|=== _ (   (   (        |\n|==_/ \\_ (   (          |\n|_/     \\_ (   (    \\__/\n          \\_ (      _/\n            |  |___/\n           /__/");
+            } else{
                 System.out.println("Error: incorrect user input. Would you like to see a cat or dog?");
                 userInput = scan.next();
                 if (userInput.toLowerCase().equals("cat")) {
                     System.out.println(" |\\__/,|   (`\\\n |_ _  |.--.) )\n ( T   )     /\n(((^_(((/(((_/");
                 } else if (userInput.toLowerCase().equals("dog")) {
                     System.out.println("            __\n(\\,--------\'()\'--o\n (_    ___    /~\"\n  (_)_)  (_)_)");
+                }else if(userInput.toLowerCase().equals("fish")){
+                    System.out.println("     o                 o\n                  o\n         o   ______      o\n           _/  (   \\_\n _       _/  (       \\_  O\n| \\_   _/  (   (    0  \\\n|== \\_/  (   (          |\n|=== _ (   (   (        |\n|==_/ \\_ (   (          |\n|_/     \\_ (   (    \\__/\n          \\_ (      _/\n            |  |___/\n           /__/");
                 }
             }
-        } while (!userInput.toLowerCase().equals("cat") && !userInput.toLowerCase().equals("dog")) ;
+        } while (!userInput.toLowerCase().equals("cat") && !userInput.toLowerCase().equals("dog")&& !userInput.toLowerCase().equals("fish")) ;
 
     }
 }
